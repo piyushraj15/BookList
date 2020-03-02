0c0d845e1e4792681f5aed9da399773031b92e88
piyushraj15 <piyush.raj73@gmail.com>
2020-03-02 17:39:36 +0530

    More functionalities to app

--- app.js
+++ app.js
@@ -1,41 +1,87 @@
 // Book class: To represent a book
 
-class Book{
-    constructor(title,author,isbn){
-        this.title=title;
-        this.author=author;
-        this.isbn=isbn;
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
     }
 
 }
 
 //UI CLASS
 
-class UI{
-    static displayBooks(){
-        const StoreBooks=[
+class UI {
+    static displayBooks() {
+        const StoreBooks = [
             {
-                title:"Book One",
-                author:"Piyush",
-                isbn:3223424
+                title: "Book One",
+                author: "Piyush",
+                isbn: 3223424
 
 
             },
             {
-                title:"Book One",
-                author:"Piyush Raj",
-                isbn:3223424
+                title: "Book One",
+                author: "Piyush Raj",
+                isbn: 3223424
 
 
             }
 
         ];
-        const books=StoreBooks;
-        books.forEach((book)=>{
+        const books = StoreBooks;
+        books.forEach((book) => {
             UI.addBookToList(book);
-        })
+        });
+
     }
+    static addBookToList(book) {
+        const list = document.querySelector("#book-list")
+        const row = document.createElement('tr')
+        row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href='#' class="btn btn-danger btn-small delete">X</a></td>`;
+
+        list.appendChild(row)
+    }
+    //clear field
+    static clearField(){
+        document.getElementById("title").value=''
+        document.getElementById("author").value=''
+        document.getElementById("isbn").value=''
+
+    }
+    //delete item
+    static deleteItem(){
+        
+
+    }
+
 }
+
+//Display Book on load
+document.addEventListener('DOMContentLoaded', UI.displayBooks)
+
+
+//Add a book to the App
+
+document.querySelector("#book-form").addEventListener("submit", (e) => {
+    e.preventDefault()
+    const target = document.getElementById("title").value
+    const author = document.getElementById("author").value
+    const isbn = document.getElementById("isbn").value
+    const newBook = new Book(target, author, isbn)
+    UI.addBookToList(newBook);
+    //clear field
+
+    UI.clearField();
+
+})
+
+
 
 
 
