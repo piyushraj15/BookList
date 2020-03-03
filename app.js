6bc804024ca5ac527fbb52fff34213bfc9f9b0d7
piyushraj15 <piyush.raj73@gmail.com>
2020-03-03 16:15:02 +0530

    Adding local storage to persist the data

--- app.js
+++ app.js
@@ -13,23 +13,24 @@ class Book {
 
 class UI {
     static displayBooks() {
-        const StoreBooks = [
-            {
-                title: "Book One",
-                author: "Piyush",
-                isbn: 3223424
+        // const StoreBooks = [
+        //     {
+        //         title: "Book One",
+        //         author: "Piyush",
+        //         isbn: 3223424
 
 
-            },
-            {
-                title: "Book One",
-                author: "Piyush Raj",
-                isbn: 3223424
+        //     },
+        //     {
+        //         title: "Book One",
+        //         author: "Piyush Raj",
+        //         isbn: 3223424
 
 
-            }
+        //     }
 
-        ];
+        // ];
+        const StoreBooks=Store.getBooks()
         const books = StoreBooks;
         books.forEach((book) => {
             UI.addBookToList(book);
@@ -50,7 +51,7 @@ class UI {
     //delete item
     static deleteItem(el){
         if(el.classList.contains('delete')){
-            el.parentElement.remove();
+            el.parentElement.parentElement.remove();
         }
 
 
@@ -61,6 +62,17 @@ class UI {
         document.getElementById("author").value=''
         document.getElementById("isbn").value=''
 
+    }
+    static showAlert(message,className){
+         const div=document.createElement('div');
+         div.className=`alert alert-${className}`;
+         div.appendChild(document.createTextNode(message));
+         const container=document.querySelector('.container');
+         const form=document.querySelector('#book-form');
+         container.insertBefore(div,form)
+
+         //Remove alert message
+         setTimeout(()=> document.querySelector(".alert").remove(),1000)
     }
     
 
@@ -77,18 +89,57 @@ document.querySelector("#book-form").addEventListener("submit", (e) => {
     const target = document.getElementById("title").value
     const author = document.getElementById("author").value
     const isbn = document.getElementById("isbn").value
-    const newBook = new Book(target, author, isbn)
-    UI.addBookToList(newBook);
-    //clear field
+    if(title==="" || author==="" || isbn===""){
+        // alert("Please fill all entery")
+        UI.showAlert("Please fill in all field",'danger')
+    }
+    else{
+        const newBook = new Book(target, author, isbn)
+        UI.addBookToList(newBook);
+        //add book to localstorage
+        Store.addBook(newBook)
+        //success message
+        UI.showAlert("Book Added","success")
+        //clear field
 
-    UI.clearField();
+
+        UI.clearField();
+    }
+    
 
 })
 
-document.getElementById('book-list').addEventListener('click',(e)=>UI.deleteItem(e.target))
+document.getElementById('book-list').addEventListener('click',(e)=>{UI.deleteItem(e.target)
+//remove from local
+Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+})
 
+class Store{
+    static getBooks(){
+        let books;
+        if(localStorage.getItem('books')===null){
+            books=[];
+        }else{
+            books=JSON.parse(localStorage.getItem('books'));
+        }
+        return books
+    }
+    static addBook(book){
+        const books=Store.getBooks();
+        books.push(book);
+        localStorage.setItem('books',JSON.stringify(books));
+    }
+    static removeBook(){
+        const books=Store.getBooks();
 
-
+        books.forEach((book,index)=>{
+            if(book.isbn===isbn){
+                books.splice(index,1)
+            }
+        });
+        localStorage.setItem('books',JSON.stringify(books))
+    }
+}
 
 
 
